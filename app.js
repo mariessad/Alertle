3535c2412b8007c710b53bbdefb97308d0f5e2a7
mariessad <74148632+mariessad@users.noreply.github.com>
2022-05-16 14:57:24 -0400

    app.js updates

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 3000;
+
 const morgan = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
 const path = require("path");
 const methodOverride = require("method-override");
 const indexRoutes = require("./routes/index-routes");
+
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs"); //this configures the app to set EJS to be the template language
 app.use(morgan("dev"));
@@ -14,6 +18,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //didn't see this in homework 12 part one but see it in 3.2 slides
 
 app.use(express.static(path.join(__dirname, "public"))); // this configures the app to know that the public directory is available
+
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(indexRoutes);
 
